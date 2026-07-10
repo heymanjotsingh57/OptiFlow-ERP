afdc27ec7e095f6aaacbd77d0c8f44454edb8fdf
Manjot Singh <heymanjotsingh57@gmail.com>
2026-07-10 16:13:17 +0530

    Added public track order page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path  # type: ignore
 from . import views
+from orders import views as order_views
 
 urlpatterns = [
     path("", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
+
+    # Public customer tracking
+    path("track-order/", order_views.track_order, name="track_order"),
 ]
